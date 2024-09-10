7e3a6b11854edb324774af835817616b30600052
FacundoLodi <estefanialodi@gmail.com>
2024-09-10 17:58:54 -0300

    ㅤ

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/NavBar/Navbar';
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <ItemListContainer />
+    </div>
+  );
+}
+
+export default App;
