c3371c44d24f526db7be2a21f2f4fc778d2b00fa
Estefania-Lodi <estefanialodi@gmail.com>
2024-10-01 16:37:49 -0300

    segunda entrega
    segunda entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import Navbar from './components/NavBar/Navbar';
-import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+import ProductDetail from './components/ProductDetail';
+import './App.css';
 
 function App() {
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
+
+  const handleCategorySelect = (category) => {
+    setCategoriaSeleccionada(category);
+  };
+
   return (
-    <div>
-      <Navbar />
-      <ItemListContainer />
-    </div>
+    <Router>
+      <div>
+        <Navbar onSelectCategory={handleCategorySelect} />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<ItemListContainer categoriaSeleccionada={categoriaSeleccionada} />} />
+          <Route path="/cremas" element={<ItemListContainer categoriaSeleccionada="cremas" />} />
+          <Route path="/maquillajes" element={<ItemListContainer categoriaSeleccionada="maquillajes" />} />
+          <Route path="/exfoliantes" element={<ItemListContainer categoriaSeleccionada="exfoliantes" />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
