62a57a204aa75958eeff2fc7d43ca56312caadce
FacundoLodi <estefanialodi@gmail.com>
2024-10-01 17:33:34 -0300

    ㅤ

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-import ItemListContainer from './components/ItemListContainer';
-import ProductDetail from './components/ProductDetail';
+import Navbar from './components/NavBar/Navbar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import './App.css';
 
-function App() {
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(null);
-
-  const handleCategorySelect = (category) => {
-    setCategoriaSeleccionada(category);
-  };
-
+const App = () => {
   return (
     <Router>
-      <div>
-        <Navbar onSelectCategory={handleCategorySelect} />
-      </div>
-      <div>
-        <Routes>
-          <Route path="/" element={<ItemListContainer categoriaSeleccionada={categoriaSeleccionada} />} />
-          <Route path="/cremas" element={<ItemListContainer categoriaSeleccionada="cremas" />} />
-          <Route path="/maquillajes" element={<ItemListContainer categoriaSeleccionada="maquillajes" />} />
-          <Route path="/exfoliantes" element={<ItemListContainer categoriaSeleccionada="exfoliantes" />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/cremas" element={<ItemListContainer categoriaSeleccionada="cremas" />} />
+        <Route path="/maquillajes" element={<ItemListContainer categoriaSeleccionada="maquillajes" />} />
+        <Route path="/exfoliantes" element={<ItemListContainer categoriaSeleccionada="exfoliantes" />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './cartWidget.css';
 
-function CartWidget({ number }) {
+const CartWidget = ({ number }) => {
   const handleClick = (event) => {
     console.log(event);
   };
 
   return (
-    <div className='containerCart'>
-      <ShoppingCartIcon className='cartIcon' onClick={handleClick} />
+    <div className='containerCart' onClick={handleClick}>
+      <ShoppingCartIcon className='cartIcon' />
       <span className='cartNumber'>{number}</span>
     </div>
   );
-}
+};
 
 export default CartWidget;
